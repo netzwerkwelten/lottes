5888676fee1b9ae6704942c809d2fd03cd92db0a
a.kohl <a.kohl@kohl-service.de>
2017-08-16 22:58:41 +0200

    update

--- src/app/root/header/header.component.ts
+++ src/app/root/header/header.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  private callapse = true;
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/shared/animation/transition.ts
+++ src/app/shared/animation/transition.ts
@@ -4,65 +4,12 @@ export function routerTransition() {
   return fade();
 }
 
-function fadeIn() {
-  trigger('routerTransition', [
-
-    // end state styles for route container (host)
-    state('*', style({
-      // the view covers the whole screen with a semi tranparent background
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: 'rgba(0, 0, 0, 0.8)'
-    })),
-
-    // route 'enter' transition
-    transition(':enter', [
-
-      // styles at start of transition
-      style({
-        // start with the content positioned off the right of the screen,
-        // -400% is required instead of -100% because the negative position adds to the width of the element
-        right: '-400%',
-
-        // start with background opacity set to 0 (invisible)
-        backgroundColor: 'rgba(0, 0, 0, 0)'
-      }),
-
-      // animation and styles at end of transition
-      animate('.5s ease-in-out', style({
-        // transition the right position to 0 which slides the content into view
-        right: 0,
-
-        // transition the background opacity to 0.8 to fade it in
-        backgroundColor: 'rgba(0, 0, 0, 0.8)'
-      }))
-    ]),
-
-    // route 'leave' transition
-    transition(':leave', [
-      // animation and styles at end of transition
-      animate('.5s ease-in-out', style({
-        // transition the right position to -400% which slides the content out of view
-        right: '-400%',
-
-        // transition the background opacity to 0 to fade it out
-        backgroundColor: 'rgba(0, 0, 0, 0)'
-      }))
-    ])
-  ]);
-}
-
 function fade() {
   return trigger('routerTransition', [
     state('void', style({
-      // the view covers the whole screen with a semi tranparent background
       left: 15,
       opacity: 1
     })),state('*', style({
-      // the view covers the whole screen with a semi tranparent background
       position: 'absolute',
       left: 15,
       opacity: 1
@@ -70,18 +17,18 @@ function fade() {
     transition(':enter', [
       style({
         position: 'fixed',
-        right: '-100%',
+        left: '100%',
         opacity: 0,
         filter: 'blur(15px)'
       }),
-      animate('1.5s ease-in-out', style({
+      animate('1.1s ease-in-out', style({
         left: 15,
         opacity: 1,
         filter: 'blur(0px)'
       }))
     ]),
     transition(':leave', [
-      animate('.6s ease-in-out', style({
+      animate('0.9s ease-in-out', style({
         left: '-100%',
         opacity: 0,
         filter: 'blur(15px)'
